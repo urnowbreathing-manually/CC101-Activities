b00a77948e7ad9c0b1778a9b26aa195d776c2460
urnowbreathing-manually <andreizantua.ucc1@gmail.com>
2024-10-11 00:37:10 +0800

    Added incorrect user input protection in menu

--- Assignment_#5/assignment5/main.c
+++ Assignment_#5/assignment5/main.c
@@ -5,7 +5,7 @@ int programNavigation();
 int rectangleAreaCalculator();
 int triangleAreaCalculator();
 
-int main(){
+int main(){ //Main Function; calls other functions in the program
     int functionSelect = 0;
     char functionTitle[3][26] = {"Rectangle Area Calculator", "Triangle Area Calculator", "Exit Program"};
 
@@ -14,31 +14,35 @@ int main(){
     printf("         CC101 - Introduction to Computing       \n");
     printf("         Zantua, Andrei I.   ||    BSCS 1A       \n\n");
 
+    //Keeps the program looping until user inputs exit code
     while(functionSelect != 3){
-        functionSelect = programNavigation(functionSelect, functionTitle);
-        switch(functionSelect){
-            case 0:
+        //Assigns return value from programNavigation to determine switch case
+        functionSelect = programNavigation(functionSelect);
+        switch(functionSelect){ //Switch case to select through functions
+            case 0: //Displays the menu at the start of program or when user inputs 0;
                 printf("Menu]\n\n");
                 break;
-            case 1:
+
+            case 1: //Calls the rectangleAreaCalculator
                 printf("%s]\n\n", functionTitle[functionSelect - 1]);
                 rectangleAreaCalculator();
                 functionSelect = 0;
                 break;
 
-            case 2:
+            case 2: //Calls the triangleAreaCalculator
                 printf("%s]\n\n", functionTitle[functionSelect - 1]);
                 triangleAreaCalculator();
                 functionSelect = 0;
                 break;
 
-            case 3:
+            case 3: //Breaks the while loop, ending the program
                 printf("%s]\n\n", functionTitle[functionSelect - 1]);
                 printf("[]===============(Program  Closed)===============[]\n");
                 break;
 
-            default:
+            default: //Displays the menu for incorrect user inputs
                 printf("Menu]\n\n");
+                functionSelect = 0;
         }
 
     }
@@ -47,6 +51,8 @@ int main(){
 }
 
 int programNavigation(int functionSelect){ //Function to help with navigation through the program
+    //Stores user input as string
+    char userInput[11];
 
     //UI text for readability
     printf("[]==================(Selection)==================[]\n");
@@ -58,10 +64,11 @@ int programNavigation(int functionSelect){ //Function to help with navigation th
     //Asks for user input to select function
     printf("[]====================(Input)====================[]\n");
     printf("  Select Function: ");
-    scanf("%d", &functionSelect);
+    fgets(userInput, 11, stdin); //Gets user input in string, with max size of 11 bytes
+    functionSelect = atoi(userInput); //Converts user input string of numbers to int values and assigns it to functionSelect
     printf("  Selected Function: [%d - ", functionSelect);
 
-    return functionSelect;
+    return functionSelect; //Returns the new value to the while loop in main()
 }
 
 int rectangleAreaCalculator(){ //Write a C program that will compute the area of a rectangle
